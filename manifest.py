ea4bb55efb2208ceee4b6262de0c2156922785bb
abdoulcherif <abdourahamane.diallo@pnlp-guinee.org>
2024-11-11 15:50:09 -0600

    Update manifest.py

--- manifest.py
+++ manifest.py
@@ -24,7 +24,7 @@ ep4_path="python_scripts"
 #requirements = "./requirements.txt"
 
 # This is where your simulations and outputs will be stored
-user = os.getlogin()
+user = os.environ.get('USER')
 user_dir= f'FE_{user}'
 job_directory = os.path.join('/projects/b1139', user_dir, 'FE-2023-examples/experiments')
 os.makedirs(job_directory, exist_ok=True)
